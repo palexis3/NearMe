9d25ce1f627ba7de5cb4410e8b2ef3ee09cbf7cd
palexis3 <alexispatrick77@gmail.com>
2017-09-23 16:45:16 -0700

    Added all necessary subfolders

--- app/src/main/java/com/example/palexis3/nearme/Activities/MainActivity.java
+++ app/src/main/java/com/example/palexis3/nearme/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.palexis3.nearme;
+package com.example.palexis3.nearme.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.palexis3.nearme.R;
 
 public class MainActivity extends AppCompatActivity {
 
