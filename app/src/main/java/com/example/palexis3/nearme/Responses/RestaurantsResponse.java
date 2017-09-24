df38bf1fbf6c251598fafe7c1454d9e97a63cad2
palexis3 <alexispatrick77@gmail.com>
2017-09-23 21:26:48 -0700

    Added RestaruantsResponse file

---
+++ app/src/main/java/com/example/palexis3/nearme/Responses/RestaurantsResponse.java
@@ -0,0 +1,29 @@
+package com.example.palexis3.nearme.Responses;
+
+import com.example.palexis3.nearme.Models.RestaurantLimitedDetails;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ RestaurantsResponse gets a list of RestaurantLimitedDetails objects.
+ */
+
+public class RestaurantsResponse {
+
+    @SerializedName("results")
+    List<RestaurantLimitedDetails> restaurantLimitedDetailsList;
+
+    public RestaurantsResponse() {
+        restaurantLimitedDetailsList = new ArrayList<>();
+    }
+
+    public static RestaurantsResponse parseJSON(String response) {
+        Gson gson = new GsonBuilder().create();
+        RestaurantsResponse restaurantsResponse = gson.fromJson(response, RestaurantsResponse.class);
+        return restaurantsResponse;
+    }
+}
